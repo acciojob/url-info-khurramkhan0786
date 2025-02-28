dd0948334fe867da80090af9710f3e535578eb37
khurram khan <153985864+khurramkhan0786@users.noreply.github.com>
2025-02-28 11:03:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
 //your JS code here. If required.
 let url = window.location.href;
-let  n = alert('print the url length');
-console.log(`The length of the URL is:${url.length}`);
+alert(`The length of the URL is:${url.length}`);
